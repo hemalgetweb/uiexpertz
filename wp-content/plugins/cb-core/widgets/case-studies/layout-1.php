0d9c5c00e577f4bd454c0ea0b8b06921457baf92
shorifahammed <shorifwork@gmail.com>
2023-08-01 17:52:21 +0600

    asfd

--- wp-content/plugins/cb-core/widgets/case-studies/layout-1.php
+++ wp-content/plugins/cb-core/widgets/case-studies/layout-1.php
@@ -11,7 +11,7 @@
                 $taxonomy = 'category'; // Assuming the taxonomy for categories is 'category', change it if needed
                 $categories = wp_get_post_terms(get_the_ID(), $taxonomy, array('fields' => 'all'));    
             ?>
-                <div class="col-md-4  col-sm-6  mb-4">
+                <div class="col-lg-4 col-md-6 col-sm-6  mb-4">
                     <div class="service-item d-flex flex-column justify-content-between h-100 bg-white">
                         <div class="uiexpertz-service-item">
                             <div class="p-1">

017828ce511f81e136f15068ff04536907d062a8
shorifahammed <shorifwork@gmail.com>
2023-08-01 16:23:00 +0600

    asdf

--- wp-content/themes/uiexpertz/assets/js/script.js
+++ wp-content/themes/uiexpertz/assets/js/script.js
@@ -105,7 +105,7 @@
                 breakpoints: {
                     // when window width is >= 320px
                     320: {
-                      slidesPerView: 3,
+                      slidesPerView: 2,
                       spaceBetween: 0
                     },
                     // when window width is >= 575px
@@ -115,16 +115,22 @@
                     },
                     // when window width is >= 640px
                     768: {
-                      slidesPerView: 5,
+                      slidesPerView: 4,
                       spaceBetween: 0
                     },
                     992: {
-                        slidesPerView: 6,
+                        slidesPerView: 5,
                         spaceBetween: 0
                     },
                     1200: {
-                        slidesPerView: 7,
+                        slidesPerView: 5,
                         spaceBetween: 0
+                    },
+                    1400: {
+                        slidesPerView: 6,
+                    },
+                    1600: {
+                        slidesPerView: 7
                     }
                 }
             });

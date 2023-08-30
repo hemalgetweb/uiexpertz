6d67bc7c038e8775da1b9bf52315ccc6ad788c45
shorifahammed <shorifwork@gmail.com>
2023-08-30 12:41:40 +0600

    pdate

--- wp-content/themes/uiexpertz/footer.php
+++ wp-content/themes/uiexpertz/footer.php
@@ -3,6 +3,12 @@
 
 <?php do_action( 'uiexpertz_footer_style' ); ?>
 
+
+<!-- back to top -->
+<button id="backToTop" class=""><svg class="mb-1" width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7.26402 0L14.528 15.5041L7.45488 12.1239L0 15.5041L7.26402 0Z" fill="white" />
+</svg>
+</button>
 <?php wp_footer(); ?>
 </body>
 </html>

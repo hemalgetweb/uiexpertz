c3a524317badb5653e137b9b33816fb6f1712dc2
Md. Asraful Hoque <asraful15-6625@diu.edu.bd>
2023-08-31 17:57:26 +0600

    blog details

--- wp-content/themes/uiexpertz/page-templates/blog-details.php
+++ wp-content/themes/uiexpertz/page-templates/blog-details.php
@@ -184,7 +184,7 @@ get_header(); ?>
        
                 <div class="uiexperts-btn-item d-flex flex-wrap mt-4 align-items-start gap-4">
                     <a class="text-decoration-none position-relative bg-btn banner-btn border-0 bg-clr-darkBlue text-white fs-14 fw-extraBold d-flex gap-2 align-items-center"
-                        href="#" target="_blank" rel="nofollow">
+                        href="#">
                         Lets’s talk <svg class="btn-icon position-absolute" width="12" height="12" viewBox="0 0 12 12"
                             fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6 12L4.9375 10.9375L9.125 6.75H0V5.25H9.125L4.9375 1.0625L6 0L12 6L6 12Z"
@@ -192,7 +192,7 @@ get_header(); ?>
                         </svg>
                     </a>
                     <a class="text-decoration-none position-relative bg-btn banner-btn border-0 bg-transparent text-clr-darkBlue fs-14 fw-extraBold d-flex gap-2 align-items-center"
-                        href="#" target="_blank" rel="nofollow">
+                        href="#">
                         Lets’s talk <svg class="btn-icon position-absolute" width="20" height="21" viewBox="0 0 20 21"
                             fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path

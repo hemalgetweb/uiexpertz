6c8b15eb76054f2f64f3865d054c20f62f359240
ashikrahman-dev <ashikur1993@gmail.com>
2023-09-14 16:39:54 +0600

    Update

--- wp-content/plugins/cb-core/widgets/testimonial/layout-1.php
+++ wp-content/plugins/cb-core/widgets/testimonial/layout-1.php
@@ -1,23 +1,25 @@
 <!-- testimonial start -->
 <div class="testimonial bg-clr-blue section-padding">
-    <div class="section-heading text-center mb-5 px-3">
-        <?php if (!empty($settings['subtitle'])): ?>
-            <div class="section-hints d-flex justify-content-center align-items-center gap-2">
-                <p class="fs-14 mb-0 fw-bold text-clr-sky">
-                    <?php echo wp_kses_post($settings['subtitle']); ?>
+    <div class="container">
+        <div class="section-heading text-center mb-5 px-3">
+            <?php if (!empty($settings['subtitle'])): ?>
+                <div class="section-hints d-flex justify-content-center align-items-center gap-2">
+                    <p class="fs-14 mb-0 fw-bold text-clr-sky">
+                        <?php echo wp_kses_post($settings['subtitle']); ?>
+                    </p>
+                </div>
+            <?php endif; ?>
+            <?php if (!empty($settings['title'])): ?>
+                <h1 class="fs-40 text-white py-2">
+                    <?php echo wp_kses_post($settings['title']); ?>
+                </h1>
+            <?php endif; ?>
+            <?php if (!empty($settings['content'])): ?>
+                <p class="text-clr-skyBlue">
+                    <?php echo wp_kses_post($settings['content']); ?>
                 </p>
-            </div>
-        <?php endif; ?>
-        <?php if (!empty($settings['title'])): ?>
-            <h1 class="fs-40 text-white py-2">
-                <?php echo wp_kses_post($settings['title']); ?>
-            </h1>
-        <?php endif; ?>
-        <?php if (!empty($settings['content'])): ?>
-            <p class="text-clr-skyBlue">
-                <?php echo wp_kses_post($settings['content']); ?>
-            </p>
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 
     <?php if (!empty($settings['slides'])): ?>
